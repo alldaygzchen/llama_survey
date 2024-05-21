97adf546395b49fd6ba3c17a930bcbf2f208713e
alldaygzchen <gonzalochen66@gmail.com>
2024-05-21 11:41:35 +0800

    ensemble retriver not done

--- Intro/ensemble_retriever.py
+++ Intro/ensemble_retriever.py
@@ -1,34 +1,85 @@
 # https://towardsdatascience.com/advanced-rag-01-small-to-big-retrieval-172181b396d4
 # https://docs.llamaindex.ai/en/stable/examples/retrievers/ensemble_retrieval/
-
-# import os 
-# os.environ["OPENAI_API_KEY"] = os.environ.get('APIKEY')
-# print("Initial Done ----------------")
-
-
-# from llama_index.core import SimpleDirectoryReader
-# from llama_index.core.node_parser import SimpleNodeParser
-# reader = SimpleDirectoryReader(input_files=["supervised.pdf"])
-# docs = reader.load_data()
-# node_parser = SimpleNodeParser.from_defaults(chunk_size=1024)
-# base_nodes = node_parser.get_nodes_from_documents(docs)
+# https://docs.llamaindex.ai/en/stable/examples/retrievers/ensemble_retrieval/
+import os 
+os.environ["OPENAI_API_KEY"] = os.environ.get('APIKEY')
+print("Initial Done ----------------")
 
 
-# sub_chunk_sizes = [128, 256, 512]
-# sub_node_parsers = [
-#     SimpleNodeParser.from_defaults(chunk_size=c) for c in sub_chunk_sizes
-# ]
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core.node_parser import SimpleNodeParser
+from llama_index.core import Document
+reader = SimpleDirectoryReader(input_files=["supervised.pdf"])
+docs0 = reader.load_data()
+doc_text = "\n\n".join([d.get_content() for d in docs0])
+docs = [Document(text=doc_text)]
+print('docs',docs)
 
-# all_nodes = []
-# for base_node in base_nodes:
-#     for n in sub_node_parsers:
-#         sub_nodes = n.get_nodes_from_documents([base_node])
-#         sub_inodes = [
-#             IndexNode.from_text_node(sn, base_node.node_id) for sn in sub_nodes
-#         ]
-#         all_nodes.extend(sub_inodes)
 
-#     # also add original node to node
-#     original_node = IndexNode.from_text_node(base_node, base_node.node_id)
-#     all_nodes.append(original_node)
-# all_nodes_dict = {n.node_id: n for n in all_nodes}
+from llama_index.llms.openai import OpenAI
+from llama_index.core.text_splitter import SentenceSplitter
+from llama_index.core import VectorStoreIndex
+
+llm = OpenAI(model="gpt-3.5-turbo")
+chunk_sizes = [1256, 512, 1024]
+nodes_list = []
+vector_indices = []
+for chunk_size in chunk_sizes:
+    print(f"Chunk Size: {chunk_size}")
+    splitter = SentenceSplitter(chunk_size=chunk_size)
+    nodes = splitter.get_nodes_from_documents(docs)
+
+    # add chunk size to nodes to track later
+    for node in nodes:
+        node.metadata["chunk_size"] = chunk_size
+        node.excluded_embed_metadata_keys = ["chunk_size"]
+        node.excluded_llm_metadata_keys = ["chunk_size"]
+
+    nodes_list.append(nodes)
+
+    # build vector index
+    vector_index = VectorStoreIndex(nodes)
+    vector_indices.append(vector_index)
+
+from llama_index.core.tools import RetrieverTool
+from llama_index.core.schema import IndexNode
+
+# retriever_tools = []
+retriever_dict = {}
+retriever_nodes = []
+for chunk_size, vector_index in zip(chunk_sizes, vector_indices):
+    node_id = f"chunk_{chunk_size}"
+    node = IndexNode(
+        text=(
+            "Retrieves relevant context from supervised.pdf (chunk size"
+            f" {chunk_size})"
+        ),
+        index_id=node_id,
+    )
+    retriever_nodes.append(node)
+    retriever_dict[node_id] = vector_index.as_retriever()
+
+from llama_index.core.selectors import PydanticMultiSelector
+
+from llama_index.core.retrievers import RouterRetriever
+from llama_index.core.retrievers import RecursiveRetriever
+from llama_index.core import SummaryIndex
+
+# the derived retriever will just retrieve all nodes
+summary_index = SummaryIndex(retriever_nodes)
+
+retriever = RecursiveRetriever(
+    root_id="root",
+    retriever_dict={"root": summary_index.as_retriever(), **retriever_dict},
+)
+
+nodes = await retriever.aretrieve(
+    "Tell me about the main aspects of safety fine-tuning"
+)
+
+
+print(f"Number of nodes: {len(nodes)}")
+for node in nodes:
+    print(node.node.metadata["chunk_size"])
+    print(node.node.get_text())
+
